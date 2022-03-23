d9636f5888a57fd9726a21cab7e82ffdfcc6ba04
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:09:18 +0100

    Update: AUTOSAR basic software service module SokFm

--- LATEST/SokFm.cpp
+++ LATEST/SokFm.cpp
@@ -40,6 +40,16 @@ class module_SokFm:
       FUNC(void, SOKFM_CODE) DeInitFunction (void);
       FUNC(void, SOKFM_CODE) GetVersionInfo (void);
       FUNC(void, SOKFM_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, SOKFM_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/

e6d89c98daa207381ee6849c66f8857aecd15f69
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:51:42 +0200

    Update: AUTOSAR basic software service module SokFm

--- LATEST/SokFm.cpp
+++ LATEST/SokFm.cpp
@@ -96,8 +96,14 @@ FUNC(void, SOKFM_CODE) module_SokFm::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_SokFm as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == SokFm_InitCheck)

f761e4ccc8951384c9cda5044d06d839688f57ce
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:36:57 +0000

    Update: AUTOSAR basic software service module SokFm

--- LATEST/SokFm.cpp
+++ LATEST/SokFm.cpp
@@ -35,6 +35,13 @@ class module_SokFm:
       public abstract_module
    ,  public class_SokFm_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_SokFm(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }

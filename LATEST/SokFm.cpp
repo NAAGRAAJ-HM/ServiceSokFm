be85cc9fe07c8654b70e64ae60d56562384261ec
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-02 12:07:43 +0200

    Update: AUTOSAR service submodule - SokFm

--- LATEST/SokFm.cpp
+++ LATEST/SokFm.cpp
@@ -72,6 +72,10 @@ CONSTP2VAR(infSchMClient, SOKFM_VAR, SOKFM_CONST) gptrinfSchMClient_SokFm = &Sok
 /******************************************************************************/
 VAR(module_SokFm, SOKFM_VAR) SokFm(
    {
+#if(STD_ON == _ReSIM)
+// char strModuleName[6];
+#else
+#endif
          SOKFM_AR_RELEASE_VERSION_MAJOR
       ,  SOKFM_AR_RELEASE_VERSION_MINOR
       ,  0x00

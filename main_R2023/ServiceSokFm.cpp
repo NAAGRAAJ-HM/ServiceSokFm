9332caae9f93ca34fc03e4b30fbb3d4374022514
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-12-27 20:37:22 +0100

    Update: AUTOSAR basic software service module for secured onboard communication - freshness manager

---
+++ main_R2023/ServiceSokFm.cpp
@@ -0,0 +1,145 @@
+/******************************************************************************/
+/* File   : ServiceSokFm.cpp                                                  */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "ServiceSokFm.hpp"
+#include "infServiceSokFm_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define SERVICESOKFM_AR_RELEASE_VERSION_MAJOR                                  4
+#define SERVICESOKFM_AR_RELEASE_VERSION_MINOR                                  3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(SERVICESOKFM_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible SERVICESOKFM_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(SERVICESOKFM_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible SERVICESOKFM_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_ServiceSokFm, SERVICESOKFM_VAR) ServiceSokFm;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, SERVICESOKFM_CODE) module_ServiceSokFm::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, SERVICESOKFM_CONST,       SERVICESOKFM_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   SERVICESOKFM_CONFIG_DATA, SERVICESOKFM_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == ServiceSokFm_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstServiceSokFm_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == ServiceSokFm_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == ServiceSokFm_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == ServiceSokFm_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SERVICESOKFM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SERVICESOKFM_CODE) module_ServiceSokFm::DeInitFunction(
+   void
+){
+#if(STD_ON == ServiceSokFm_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceSokFm_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == ServiceSokFm_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SERVICESOKFM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SERVICESOKFM_CODE) module_ServiceSokFm::MainFunction(
+   void
+){
+#if(STD_ON == ServiceSokFm_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceSokFm_InitCheck)
+   }
+   else{
+#if(STD_ON == ServiceSokFm_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SERVICESOKFM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+

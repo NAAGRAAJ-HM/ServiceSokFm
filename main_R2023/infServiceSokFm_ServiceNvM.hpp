8e32a176cc9fa60b0b627a77ea37579406ed287e
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:49:56 +0100

    Update: AUTOSAR basic software service module for secured onboard communication - freshness manager

---
+++ main_R2023/infServiceSokFm_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceSokFm_ServiceNvM.hpp                                    */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICESOKFM_AR_RELEASE_VERSION_MAJOR                             4
+#define CONSTSERVICESOKFM_AR_RELEASE_VERSION_MINOR                             3
+
+#define Const_DefaultInit_ConstServiceSokFm                                                                                \
+   ,  {CONSTSERVICESOKFM_AR_RELEASE_VERSION_MAJOR, CONSTSERVICESOKFM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
